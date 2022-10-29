703f4731ab2970fc1f695d07609700a775802292
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-10-30 04:26:40 +0900

    feat :: jwt 검증 필터 생성 및 security filter chain 앞에 넣음

---
+++ src/main/java/com/java/recode/global/security/FilterConfig.java
@@ -0,0 +1,23 @@
+package com.java.recode.global.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.java.recode.global.security.jwt.JwtFilter;
+import com.java.recode.global.security.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class FilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final JwtProvider jwtProvider;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void configure(HttpSecurity builder) throws Exception {
+        JwtFilter jwtFilter = new JwtFilter(jwtProvider);
+        builder.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
