12c97726e930e0092c09e74efadcb64c0cd4f6aa
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-10-30 04:19:57 +0900

    feat :: JwtProvider

--- src/main/java/com/java/recode/global/security/jwt/JwtProvider.java
+++ src/main/java/com/java/recode/global/security/jwt/JwtProvider.java
@@ -2,12 +2,20 @@ package com.java.recode.global.security.jwt;
 
 import com.java.recode.domain.auth.domain.repository.RefreshTokenRepository;
 import com.java.recode.global.security.auth.AuthDetailsService;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.parameters.P;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
+import java.time.ZonedDateTime;
 import java.util.Date;
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
@@ -35,4 +43,41 @@ public class JwtProvider {
         return generatedToken(id, jwtProperties.getRefreshExp());
     }
 
+    public String resolveToken(HttpServletRequest request) {
+        String bearer = request.getHeader(jwtProperties.getHeader());
+        return parseToken(bearer);
+    }
+
+    public String parseToken(String bearerToken) {
+        return bearerToken != null && bearerToken.startsWith(jwtProperties.getPrefix()) ?
+                bearerToken.replace(jwtProperties.getPrefix(), "") :
+                null;
+    }
+
+    public ZonedDateTime getExpiredTime() {
+        return ZonedDateTime.now().plusSeconds(jwtProperties.getAccessExp());
+    }
+
+    public Authentication authentication(String token) {
+        UserDetails userDetails = authDetailsService
+                .loadUserByUsername(getTokenSubject(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    private String getTokenSubject(String token) {
+        return Objects.requireNonNull(getTokenBody(token)).getSubject();
+    }
+
+    private Claims getTokenBody(String token) {
+        try {
+            return Jwts.parser().setSigningKey(jwtProperties.getSecretKey())
+                    .parseClaimsJws(token).getBody();
+        } catch (io.jsonwebtoken.ExpiredJwtException e) {
+//            throw ExpiredJwtException.EXCEPTION;
+        } catch (Exception e) {
+//            throw InvalidJwtException.EXCEPTION;
+        }
+
+        return null;
+    }
 }
