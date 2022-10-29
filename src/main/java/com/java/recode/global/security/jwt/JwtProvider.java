3a258cd485c67aa61e0b8632ffe3696083ba53ba
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-10-30 04:10:06 +0900

    feat :: 토큰 생성, 액세스 토큰 생성

--- src/main/java/com/java/recode/global/security/jwt/JwtProvider.java
+++ src/main/java/com/java/recode/global/security/jwt/JwtProvider.java
@@ -2,8 +2,12 @@ package com.java.recode.global.security.jwt;
 
 import com.java.recode.domain.auth.domain.repository.RefreshTokenRepository;
 import com.java.recode.global.security.auth.AuthDetailsService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
@@ -13,4 +17,17 @@ public class JwtProvider {
     private final AuthDetailsService authDetailsService;
     private final RefreshTokenRepository refreshTokenRepository;
 
+    public String generatedAccessToken(String id) {
+        return generatedToken(id, jwtProperties.getAccessExp());
+    }
+
+    private String generatedToken(String id, Long exp) {
+        return Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .setSubject(id)
+                .claim("type", "access")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + exp * 1000))
+                .compact();
+    }
 }
